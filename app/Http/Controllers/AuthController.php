21e99e349ec7dcb292b6fd3b1d515f71ed26485b
batatu <herisvan321@gmail.com>
2026-03-26 01:23:58 +0700

    nevadra-pustaka3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/');
+            return redirect()->intended('/admin');
         }
 
         return back()->withErrors([
