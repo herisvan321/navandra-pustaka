ee81ad6db0c84e1f193093282598ab2b1cef3d19
batatu <herisvan321@gmail.com>
2026-04-05 19:28:24 +0700

    baru2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Admin Nevandra',
                 'password' => Hash::make('12345678'),
+                'role' => 'admin',
             ]
         );
 
